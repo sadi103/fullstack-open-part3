5595bfad081e4bc21233f5d253ab8cca7f8e2553
Ibrahim Sadi <ibrahimsadi103@gmail.com>
2024-11-12 16:36:16 +0200

    fullstack open exercise 3.10

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${currentDate}</p>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
